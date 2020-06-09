47ffbfc4eff8d85d4053a9c48773209f33cdda85
Amr Sekilly <amr@pelcro.com>
2020-06-09 19:14:59 +0200

    Enhanced the demo page

--- src/App.js
+++ src/App.js
@@ -11,39 +11,48 @@ function App() {
       </h1>
       <div className="flexbox">
         <div>
-          <NoBorderSpinner size={60} color="red" stroke={3} duration={700} />
+          <h2 className="item-title">NoBorderSpinner</h2>
+          <NoBorderSpinner size={70} color="red" stroke={4} duration={700} />
         </div>
 
         <div>
+          <h2 className="item-title">BorderSpinner</h2>
           <div className="bt-spinner"></div>
         </div>
 
         <div>
+          <h2 className="item-title">DoubleBorderSpinner</h2>
           <div className="db-spinner"></div>
         </div>
 
         <div>
+          <h2 className="item-title">DoubleSpinner</h2>
           <div className="dbl-spinner"></div>
           <div className="dbl-spinner dbl-spinner--2"></div>
         </div>
 
         <div>
+          <h2 className="item-title">ReverseSpinner</h2>
           <div className="reverse-spinner"></div>
         </div>
 
         <div>
+          <h2 className="item-title">HMSpinner</h2>
           <div className="hm-spinner"></div>
         </div>
 
         <div>
+          <h2 className="item-title">TripleSpinner</h2>
           <div className="triple-spinner"></div>
         </div>
 
         <div>
+          <h2 className="item-title">CMSpinner</h2>
           <div className="cm-spinner"></div>
         </div>
 
         <div>
+          <h2 className="item-title">MultiSpinner</h2>
           <div className="multi-spinner-container">
             <div className="multi-spinner">
               <div className="multi-spinner">
@@ -60,12 +69,14 @@ function App() {
         </div>
 
         <div>
+          <h2 className="item-title">DotLoader</h2>
           <div className="dot-loader"></div>
           <div className="dot-loader dot-loader--2"></div>
           <div className="dot-loader dot-loader--3"></div>
         </div>
 
         <div>
+          <h2 className="item-title">CircleLoader</h2>
           <div className="circle-loader">
             <div></div>
             <div></div>
@@ -79,6 +90,7 @@ function App() {
         </div>
 
         <div>
+          <h2 className="item-title">MLLoader</h2>
           <div className="ml-loader">
             <div></div>
             <div></div>
@@ -96,6 +108,7 @@ function App() {
         </div>
 
         <div>
+          <h2 className="item-title">MeshLoader</h2>
           <div className="mesh-loader">
             <div className="set-one">
               <div className="circle"></div>
@@ -109,10 +122,12 @@ function App() {
         </div>
 
         <div>
+          <h2 className="item-title">HeartLoader</h2>
           <div className="heart-loader"></div>
         </div>
 
         <div>
+          <h2 className="item-title">TriForceLoader</h2>
           <div className="triforce-container">
             <div className="triforce"></div>
           </div>
