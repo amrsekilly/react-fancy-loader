62c49131748076afa17b402661709a94415be79e
Amr Sekilly <amr@pelcro.com>
2020-06-10 20:06:56 +0200

    Increased the specificity of the styles

--- src/Components/Loaders/DotLoader.js
+++ src/Components/Loaders/DotLoader.js
@@ -4,34 +4,41 @@ import { Grow } from "../Styles/Grow";
 import { FlexDiv } from "../Styles/FlexDiv";
 
 const FirstDot = styled(Grow)`
-  border-radius: 50% !important;
-  position: relative !important;
+  &&&&&&&&&&&& {
+    border-radius: 50% !important;
+    position: relative !important;
 
-  ${({ size = 75, color = "#009688", duration = 1200 }) =>
-    css`
-      width: ${size}px !important;
-      height: ${size}px !important;
-      background-color: ${color} !important;
-      -webkit-animation: ${duration / 1000}s grow ease-in-out infinite !important;
-      animation: ${duration / 1000}s grow ease-in-out infinite !important;
-    `};
+    ${({ size = 75, color = "#009688", duration = 1200 }) =>
+      css`
+        width: ${size}px !important;
+        height: ${size}px !important;
+        background-color: ${color} !important;
+        -webkit-animation: ${duration / 1000}s grow ease-in-out infinite !important;
+        animation: ${duration / 1000}s grow ease-in-out infinite !important;
+      `};
+  }
 `;
 
 const SecondDot = styled(FirstDot)`
-  ${({ duration = 1200, size = 75 }) =>
-    css`
-      margin: 0 ${size / 3}px !important;
-      -webkit-animation: ${duration / 1000}s grow ease-in-out infinite 0.15555s !important;
-      animation: ${duration / 1000}s grow ease-in-out infinite 0.15555s !important;
-    `};
+  &&&&&&&&&&&& {
+    ${({ duration = 1200, size = 75 }) =>
+      css`
+        margin: 0 ${size / 3}px !important;
+        -webkit-animation: ${duration / 1000}s grow ease-in-out infinite
+          0.15555s !important;
+        animation: ${duration / 1000}s grow ease-in-out infinite 0.15555s !important;
+      `};
+  }
 `;
 
 const ThirdDot = styled(FirstDot)`
-  ${({ duration = 1200 }) =>
-    css`
-      -webkit-animation: ${duration / 1000}s grow ease-in-out infinite 0.3s !important;
-      animation: ${duration / 1000}s grow ease-in-out infinite 0.3s !important;
-    `};
+  &&&&&&&&&&&& {
+    ${({ duration = 1200 }) =>
+      css`
+        -webkit-animation: ${duration / 1000}s grow ease-in-out infinite 0.3s !important;
+        animation: ${duration / 1000}s grow ease-in-out infinite 0.3s !important;
+      `};
+  }
 `;
 
 export const DotLoader = (props) => (
--- src/Components/Loaders/NoBorderSpinner.js
+++ src/Components/Loaders/NoBorderSpinner.js
@@ -2,17 +2,19 @@ import styled, { css } from "styled-components";
 import { Spinner } from "../Styles/Spinner";
 
 export const NoBorderSpinner = styled(Spinner)`
-  margin: 0 !important;
-  background: transparent !important;
-  border-right: 4px solid transparent !important;
-  border-radius: 50% !important;
+  &&&&&&&&&&&& {
+    margin: 0 !important;
+    background: transparent !important;
+    border-right: 4px solid transparent !important;
+    border-radius: 50% !important;
 
-  ${({ size = 75, color = "#009688", stroke = 4, duration = 1000 }) =>
-    css`
-      width: ${size}px !important;
-      height: ${size}px !important;
-      border-top: ${stroke}px solid ${color} !important;
-      -webkit-animation: ${duration / 1000}s spin linear infinite !important;
-      animation: ${duration / 1000}s spin linear infinite !important;
-    `};
+    ${({ size = 75, color = "#009688", stroke = 4, duration = 1000 }) =>
+      css`
+        width: ${size}px !important;
+        height: ${size}px !important;
+        border-top: ${stroke}px solid ${color} !important;
+        -webkit-animation: ${duration / 1000}s spin linear infinite !important;
+        animation: ${duration / 1000}s spin linear infinite !important;
+      `};
+  }
 `;
