2d278fa3d2d9a36c84800d4c5643be66c359ef1e
Amr Sekilly <amr@pelcro.com>
2020-06-10 17:13:57 +0200

    Fixed the styles from being overwritten

--- src/Components/Loaders/DotLoader.js
+++ src/Components/Loaders/DotLoader.js
@@ -4,33 +4,33 @@ import { Grow } from "../Styles/Grow";
 import { FlexDiv } from "../Styles/FlexDiv";
 
 const FirstDot = styled(Grow)`
-  border-radius: 50%;
-  position: relative;
+  border-radius: 50% !important;
+  position: relative !important;
 
   ${({ size = 75, color = "#009688", duration = 1200 }) =>
     css`
-      width: ${size}px;
-      height: ${size}px;
-      background-color: ${color};
-      -webkit-animation: ${duration / 1000}s grow ease-in-out infinite;
-      animation: ${duration / 1000}s grow ease-in-out infinite;
+      width: ${size}px !important;
+      height: ${size}px !important;
+      background-color: ${color} !important;
+      -webkit-animation: ${duration / 1000}s grow ease-in-out infinite !important;
+      animation: ${duration / 1000}s grow ease-in-out infinite !important;
     `};
 `;
 
 const SecondDot = styled(FirstDot)`
   ${({ duration = 1200, size = 75 }) =>
     css`
-      margin: 0 ${size / 3}px;
-      -webkit-animation: ${duration / 1000}s grow ease-in-out infinite 0.15555s;
-      animation: ${duration / 1000}s grow ease-in-out infinite 0.15555s;
+      margin: 0 ${size / 3}px !important;
+      -webkit-animation: ${duration / 1000}s grow ease-in-out infinite 0.15555s !important;
+      animation: ${duration / 1000}s grow ease-in-out infinite 0.15555s !important;
     `};
 `;
 
 const ThirdDot = styled(FirstDot)`
   ${({ duration = 1200 }) =>
     css`
-      -webkit-animation: ${duration / 1000}s grow ease-in-out infinite 0.3s;
-      animation: ${duration / 1000}s grow ease-in-out infinite 0.3s;
+      -webkit-animation: ${duration / 1000}s grow ease-in-out infinite 0.3s !important;
+      animation: ${duration / 1000}s grow ease-in-out infinite 0.3s !important;
     `};
 `;
 
--- src/Components/Loaders/NoBorderSpinner.js
+++ src/Components/Loaders/NoBorderSpinner.js
@@ -2,17 +2,17 @@ import styled, { css } from "styled-components";
 import { Spinner } from "../Styles/Spinner";
 
 export const NoBorderSpinner = styled(Spinner)`
-  margin: 0;
-  background: transparent;
-  border-right: 4px solid transparent;
-  border-radius: 50%;
+  margin: 0 !important;
+  background: transparent !important;
+  border-right: 4px solid transparent !important;
+  border-radius: 50% !important;
 
   ${({ size = 75, color = "#009688", stroke = 4, duration = 1000 }) =>
     css`
-      width: ${size}px;
-      height: ${size}px;
-      border-top: ${stroke}px solid ${color};
-      -webkit-animation: ${duration / 1000}s spin linear infinite;
-      animation: ${duration / 1000}s spin linear infinite;
+      width: ${size}px !important;
+      height: ${size}px !important;
+      border-top: ${stroke}px solid ${color} !important;
+      -webkit-animation: ${duration / 1000}s spin linear infinite !important;
+      animation: ${duration / 1000}s spin linear infinite !important;
     `};
 `;
